d83f7ecc7be411aea54008e00cc34397d929a0c1
Jaco Prinsloo <jmprinsloo@gmail.com>
2012-01-18 17:44:38 +0200

    Cleaned the project's spacing and added usage instructions.

--- db/evolutions/1.sql
+++ db/evolutions/1.sql
@@ -3,14 +3,14 @@
 # --- !Ups
 
 CREATE TABLE user (
-    id BIGINT(20) NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
+  id BIGINT(20) NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
 );
 
 CREATE UNIQUE INDEX user_name
-    ON user (name);
+  ON user (name);
 
 # --- !Downs
 
